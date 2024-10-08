8c2ff22545ffa29c31f3ce478e547e0db604bdbc
Rizky Yusmansyah <yusmansyah@mhs.usk.ac.id>
2024-10-08 19:34:20 +0700

    update

--- newsportal/admin/includes/footer.php
+++
@@ -1,4 +0,0 @@
-
-                <footer class="footer text-right">
-                   <?php echo date('Y');?> © News Portal.
-                </footer>
--- newsportal/includes/footer.php
+++
@@ -1,6 +0,0 @@
-    <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; NewPortal <?php echo date('Y');?></p>
-      </div>
-      <!-- /.container -->
-    </footer>
